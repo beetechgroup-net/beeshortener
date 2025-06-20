880d1c6d5bfe26c6867efa05281986764392ea8a
Gabriel Menezes <bolsista.senaiemp18@ms.senai.br>
2025-06-20 09:37:39 -0400

    alterando o endpoint

--- backend/src/main/java/net/beetechgroup/resource/encode/EncodeOutput.java
+++ backend/src/main/java/net/beetechgroup/resource/encode/EncodeOutput.java
@@ -1,10 +1,10 @@
 package net.beetechgroup.resource.encode;
 
-import io.quarkus.runtime.annotations.RegisterForReflection;
 import java.time.LocalDateTime;
 import lombok.Data;
 import net.beetechgroup.domain.URL;
 
+@Data
 public class EncodeOutput {
     private Long id;
     private Boolean active;
@@ -13,9 +13,6 @@ public class EncodeOutput {
     private String longURL;
     private String code;
 
-    public EncodeOutput() {
-    }
-
     public EncodeOutput(URL url) {
         this.id = url.getId();
         this.active = url.getActive();
@@ -23,53 +20,5 @@ public class EncodeOutput {
         this.updateAt = url.getUpdateAt();
         this.longURL = url.getLongURL();
         this.code = url.getCode();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    public LocalDateTime getCreateAt() {
-        return createAt;
-    }
-
-    public void setCreateAt(LocalDateTime createAt) {
-        this.createAt = createAt;
-    }
-
-    public LocalDateTime getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(LocalDateTime updateAt) {
-        this.updateAt = updateAt;
-    }
-
-    public String getLongURL() {
-        return longURL;
-    }
-
-    public void setLongURL(String longURL) {
-        this.longURL = longURL;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
     }
 }
--- backend/src/main/java/net/beetechgroup/resource/encode/EncodeResource.java
+++ backend/src/main/java/net/beetechgroup/resource/encode/EncodeResource.java
@@ -6,9 +6,10 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
 import net.beetechgroup.service.SaveURL;
+import org.jboss.resteasy.reactive.RestResponse;
+import org.jboss.resteasy.reactive.RestResponse.ResponseBuilder;
 
 @ApplicationScoped
 @Produces(MediaType.APPLICATION_JSON)
@@ -19,9 +20,11 @@ public class EncodeResource {
 
     private final SaveURL saveURL;
 
+
     @POST
-    public Response encode(EncodeInput encodeInput) {
-        return Response.ok(saveURL.execute(encodeInput)).build();
+    public RestResponse<EncodeOutput> encode(EncodeInput encodeInput) {
+        EncodeOutput output = saveURL.execute(encodeInput);
+        return ResponseBuilder.ok(output).build();
     }
 
 }
